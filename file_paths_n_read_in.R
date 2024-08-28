5cebce5d2a781bbe8178bd772df097ba2288a7b2
jpcohen <jpc071997@gmail.com>
2024-08-28 22:22:45 +0000

    did a thing idk

--- file_paths_n_read_in.R
+++ file_paths_n_read_in.R
@@ -2,6 +2,11 @@ library(tidyverse)
 
 group_fold_path <- "/courses/EDS214/group_project/2024/dream-stream-team"
 r_proj_fold_path <- "~/eds_214_group_project/eds214-dream-stream-team-group-project"
-a1_path <- "/courses/EDS214/group_project/2023/A1"
+raw_data_path <- "/courses/EDS214/group_project/2024/dream_stream_team/raw_data"
 
-tt <- read_csv(file.path(a1_path, "BasicFieldData-Rain2.csv"))
+raw_data1 <- read_csv(file.path(raw_data_path, "QuebradaCuenca1-Bisley.csv"))
+raw_data2 <- read_csv(file.path(raw_data_path, "QuebradaCuenca2-Bisley.csv"))
+raw_data3 <- read_csv(file.path(raw_data_path, "QuebradaCuenca3-Bisley.csv"))
+raw_data4 <- read_csv(file.path(raw_data_path, "RioMameyesPuenteRoto.csv"))
+
+full_join(raw_data1, raw_data2, by = join_by(Code))
