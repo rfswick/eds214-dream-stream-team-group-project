a466490300f0e38cea7283808d243e7756059674
jpcohen <jpc071997@gmail.com>
2024-08-28 21:28:17 +0000

    loaded in data

--- file_paths.R
+++ file_paths.R
@@ -2,3 +2,6 @@ library(tidyverse)
 
 group_fold_path <- "/courses/EDS214/group_project/2024/dream-stream-team"
 r_proj_fold_path <- "~/eds_214_group_project/eds214-dream-stream-team-group-project"
+a1_path <- "/courses/EDS214/group_project/2023/A1"
+
+tt <- read_csv(file.path(a1_path, "BasicFieldData-Rain2.csv"))
